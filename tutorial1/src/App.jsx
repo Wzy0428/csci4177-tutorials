4e6129f10571498b1df61762e6c321521355fc26
zhiyingw <zh617138@dal.ca>
2026-01-24 12:46:43 -0400

    Add Tutorial 1 with Vite React

---
+++ tutorial1/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Tutorial 1 - It Works!</h1>
+      <p>CSCI4177 - Winter 2026</p>
+    </>
+  )
+}
+
+export default App
